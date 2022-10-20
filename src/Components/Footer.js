ab4386abb80471036b174354c80c4f4a68f67a36
tanu-kelde <tanu@thoughtwin.com>
2022-10-20 12:49:15 +0530

    home page api call code

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -40,56 +40,56 @@ function Footer() {
       <div className="row mt-5">
         <h6 className="h6">Product tags</h6>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Beans</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Beans</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Carrots</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Carrots</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Apples</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Apples</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Garlic</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Garlic</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Mushrooms</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Mushrooms</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Tomatoes</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Tomatoes</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Chilli peppers</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Chilli</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Broccoli</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Broccoli</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Watermelons</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Watermelons</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Oranges</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Oranges</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Bananas</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Bananas</Button>
         </div>
         <div className="col-1">
-          <Button variant="light" style={{"font-size":"15px"}}>Grapes</Button>
+          <Button variant="light" style={{"fontSize":"15px"}}>Grapes</Button>
         </div>
       </div>
       <div className="row">
         <div className="col-6">
-        <Button variant="light" style={{"font-size":"15px"}}>Cherries</Button>
-        <Button variant="light" style={{"font-size":"15px"}}>Meat</Button>
-        <Button variant="light" style={{"font-size":"15px"}}>Seo tag</Button>
-        <Button variant="light" style={{"font-size":"15px"}}>Fish</Button>
-        <Button variant="light" style={{"font-size":"15px"}}>Seo tag</Button>
-        <Button variant="light" style={{"font-size":"15px"}}>Fresh food</Button>
-        <Button variant="light" style={{"font-size":"15px"}}>Lemons</Button>
+        <Button variant="light" style={{"fontSize":"15px"}}>Cherries</Button>
+        <Button variant="light" style={{"fontSize":"15px"}}>Meat</Button>
+        <Button variant="light" style={{"fontSize":"15px"}}>Seo tag</Button>
+        <Button variant="light" style={{"fontSize":"15px"}}>Fish</Button>
+        <Button variant="light" style={{"fontSize":"15px"}}>Seo tag</Button>
+        <Button variant="light" style={{"fontSize":"15px"}}>Fresh food</Button>
+        <Button variant="light" style={{"fontSize":"15px"}}>Lemons</Button>
         </div>
        
       </div>
       <div>
-      <Card.Text style={{color:"#575757","font-size":"15px"}} className="mt-3">Copyright © 2020 petrbilek.com</Card.Text>
+      <Card.Text style={{color:"#575757","fontSize":"15px"}} className="mt-3">Copyright © 2020 petrbilek.com</Card.Text>
       </div>
     </div>
   );
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -4,9 +4,25 @@ import Card from "react-bootstrap/Card";
 import "primeicons/primeicons.css";
 import { BsChevronRight } from "react-icons/bs";
 import { logo, stock, natural } from "../assets/images";
-import {BsSquare} from 'react-icons/bs';
+import { BsSquare } from "react-icons/bs";
+import { propTypes } from "react-bootstrap/esm/Image";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function HomePage() {
+  const baseURL = "https://freshness12.herokuapp.com/user/userdata";
+
+  const [post, setPost] = React.useState([]);
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  const loadProducts = async () => {
+    const result = await axios.get(baseURL);
+    setPost(result.data);
+  };
+
   return (
     <>
       <div className="container mt-5">
@@ -99,49 +115,25 @@ function HomePage() {
               More Products <BsChevronRight />{" "}
             </Button>
           </div>
-          <div className="col-3">
-            <Card style={{ width: "15rem" }}>
-              <Card.Img variant="top" src={stock} />
-              <Card.Body>
-                <Card.Title className="title">Product Title</Card.Title>
-                <Card.Text className="description">
-                  Space for a small product description
-                </Card.Text>
-                <Button variant="light" className="USDButton">
-                  1.48 USD
-                </Button>
-                <Button className="buyNow">Buy Now</Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-3">
-            <Card style={{ width: "15rem" }}>
-              <Card.Img variant="top" src={logo} />
-              <Card.Body>
-                <Card.Title className="title">Product Title</Card.Title>
-                <Card.Text className="description">
-                  Space for a small product description
-                </Card.Text>
-                <Button variant="light">0.28 USD </Button>
-                <Button className="buyNow">Buy Now</Button>
-                <p className="price">48.56</p>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-3">
-            <Card style={{ width: "15rem" }}>
-              <Card.Img variant="top" src={natural} />
-              <Card.Body>
-                <Card.Title className="title">Product Title</Card.Title>
-                <Card.Text className="description">
-                  Space for a small product description
-                </Card.Text>
-                <Button variant="light">1.12 USD</Button>
-                <Button className="buyNow">Buy Now</Button>
-                <p className="price">48.56</p>
-              </Card.Body>
-            </Card>
-          </div>
+          {
+            post.map((m) => ( 
+              <div key={m['_id']} className="col-3">
+                  <Card style={{ width: "15rem" }}>
+                    <Card.Img variant="top" src={m?.Product_Image} />
+                    <Card.Body>
+                      <Card.Title className="title">{m?.Product_title}</Card.Title>
+                      <Card.Text className="description">
+                        {m?.Product_discription}
+                      </Card.Text>
+                      <Button variant="light" className="USDButton">
+                        {m?.Product_price}
+                      </Button>
+                      <Button className="buyNow">Buy Now</Button>
+                    </Card.Body>
+                  </Card>
+              </div>
+            ))
+          }
         </div>
       </div>
       <div className="container mt-5">
@@ -342,46 +334,60 @@ function HomePage() {
         </div>
         <div className="row">
           <div className="col-4 cardColor">
-          <Card.Text className="vagetable">Dinner tips</Card.Text>
+            <Card.Text className="vagetable">Dinner tips</Card.Text>
             <Card
               style={{ width: "18rem", height: "150px" }}
               className="blogCard"
             >
               <Card.Body className="cardBody">
-                <Card.Text className="descriptionlast" style={{color:"white"}}>
+                <Card.Text
+                  className="descriptionlast"
+                  style={{ color: "white" }}
+                >
                   Our chef tips for a great and tasty dinner ready in 20 minutes
                 </Card.Text>
-                <p className="paragraph" style={{color:"white"}}>Author 17.6.2020</p>
+                <p className="paragraph" style={{ color: "white" }}>
+                  Author 17.6.2020
+                </p>
               </Card.Body>
-              
             </Card>
           </div>
           <div className="col-3">
             <Card.Img variant="top" src={stock} />
             <Card.Text className="vagetable">Vagetable</Card.Text>
             <Card.Text className="descriptionlast">
-            Which vegetable your family will love and want’s eat each day
+              Which vegetable your family will love and want’s eat each day
             </Card.Text>
             <p className="paragraph2">Author 15.6.2020</p>
           </div>
           <div className="col-3">
             <Card.Text>
-            Salat is kinda good start to your morning routines
+              Salat is kinda good start to your morning routines
             </Card.Text>
             <p className="paragraph2">Author 15.6.2020</p>
             <Card.Text className="mt-5">
-            Salat is kinda good start to your morning routines
+              Salat is kinda good start to your morning routines
             </Card.Text>
             <p className="paragraph2">Author 15.6.2020</p>
             <Card.Text className="mt-5">
-            Salat is kinda good start to your morning routines
+              Salat is kinda good start to your morning routines
             </Card.Text>
             <p className="paragraph2">Author 15.6.2020</p>
           </div>
           <div className="col-2 mt-5">
-            <span className=" mt-4 icons" style={{'margin-top':'30px'}} ><BsSquare size={70} /><br/></span><br></br>
-            <span className="mt-5 icons"><BsSquare size={70}/><br/></span><br></br>
-            <span className="mt-5 icons"><BsSquare size={70}/></span>
+            <span className=" mt-4 icons" style={{ "marginTop": "30px" }}>
+              <BsSquare size={70} />
+              <br />
+            </span>
+            <br></br>
+            <span className="mt-5 icons">
+              <BsSquare size={70} />
+              <br />
+            </span>
+            <br></br>
+            <span className="mt-5 icons">
+              <BsSquare size={70} />
+            </span>
           </div>
         </div>
       </div>
